71522a9d0ea4cc9b66d8abdd63e789c06dfc2fcb
ArpanMaheshwari <maheshwariarpan828@gmail.com>
2021-05-29 15:56:19 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let fetchData = document.getElementById("fetch");
+let tempHead = document.getElementById("temp");
+let card = document.getElementById("contain");
+
+card.style.display = "none";
+
+fetchData.addEventListener("click", () => {
+
+    let location = document.getElementById("location").value;
+
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c7d37383f37163c9f9f3e77ce2bccde7&units=metric`;
+
+    fetch(url).then(async (response) => {
+
+        const wData = await response.json();
+        const temp = wData.main.temp;
+        const main = wData.weather[0].main;
+        const place = wData.name;
+
+        const t = document.getElementById("temp");
+        const d = document.getElementById("desc");
+        const p = document.getElementById("place");
+
+
+        card.style.display = "block";
+
+        t.innerHTML = temp + " &deg;C";
+        d.innerHTML = "You will observe " + main + "!";
+        p.innerHTML = place;
+
+    }).catch(err => {
+        console.log(err);
+    })
+
+})
