15400f0ec32fc0961ba05a1eca63c2cf3cf1213d
ArpanMaheshwari <maheshwariarpan828@gmail.com>
2021-09-05 20:19:37 +0530

    First Commit

--- app.js
+++ app.js
@@ -5,31 +5,28 @@ let card = document.getElementById("contain");
 card.style.display = "none";
 
 fetchData.addEventListener("click", () => {
+  let location = document.getElementById("location").value;
 
-    let location = document.getElementById("location").value;
+  url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c7d37383f37163c9f9f3e77ce2bccde7&units=metric`;
 
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c7d37383f37163c9f9f3e77ce2bccde7&units=metric`;
+  fetch(url)
+    .then(async (response) => {
+      const wData = await response.json();
+      const temp = wData.main.temp;
+      const main = wData.weather[0].main;
+      const place = wData.name;
 
-    fetch(url).then(async (response) => {
+      const t = document.getElementById("temp");
+      const d = document.getElementById("desc");
+      const p = document.getElementById("place");
 
-        const wData = await response.json();
-        const temp = wData.main.temp;
-        const main = wData.weather[0].main;
-        const place = wData.name;
+      card.style.display = "block";
 
-        const t = document.getElementById("temp");
-        const d = document.getElementById("desc");
-        const p = document.getElementById("place");
-
-
-        card.style.display = "block";
-
-        t.innerHTML = temp + " &deg;C";
-        d.innerHTML = "You will observe " + main + "!";
-        p.innerHTML = place;
-
-    }).catch(err => {
-        console.log(err);
+      t.innerHTML = temp + " &deg;C";
+      d.innerHTML = "You will observe " + main + "!";
+      p.innerHTML = place;
     })
-
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
